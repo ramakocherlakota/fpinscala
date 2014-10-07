79cf78e65b6dd7eeab565399718bee3c443cea73
Rama Kocherlakota <Rama.Kocherlakota@dealer.com>
2014-10-07 16:57:09 -0400

    starting Stream work

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -12,14 +12,32 @@ trait Stream[+A] {
   def exists(p: A => Boolean): Boolean = 
     foldRight(false)((a, b) => p(a) || b) // Here `b` is the unevaluated recursive step that folds the tail of the stream. If `p(a)` returns `true`, `b` will never be evaluated and the computation terminates early.
 
+  def toList() : List[A] = 
+    foldRight[List[A]](Nil:List[A])((h, t) => h :: t)
+
   @annotation.tailrec
   final def find(f: A => Boolean): Option[A] = this match {
     case Empty => None
     case Cons(h, t) => if (f(h())) Some(h()) else t().find(f)
   }
-  def take(n: Int): Stream[A] = sys.error("todo")
 
-  def drop(n: Int): Stream[A] = sys.error("todo")
+  def take(n: Int): Stream[A] = {
+    if (n <= 0)
+      Empty
+    else this match {
+      Empty => Empty
+      Cons(h, t) => Cons(h, t.take(n-1))
+    }
+  }
+
+  def drop(n: Int): Stream[A] = this match {
+    if (n <= 0)
+      this
+    else this match {
+      Empty => Empty
+      Cons(h, t) =>  t.drop(n-1))
+    }
+  }
 
   def takeWhile(p: A => Boolean): Stream[A] = sys.error("todo")
 
